a80b1ae6a7bd33e08f89e0b58cd5d03f39b8376d
Tan Le-Dang <tledang@rsglab.com>
2020-03-09 11:03:32 -0400

    made changes to the watchlist to track movies you've watched as well

--- watchlist.js
+++ watchlist.js
@@ -1,10 +1,24 @@
 function removeWatchlist(imdbID){ 
-    var movie = JSON.parse(localStorage.getItem("watchlist")).findIndex(function(currentMovie){
+    console.log(localStorage.getItem("watchlist"));
+    var movie = JSON.parse(localStorage.getItem("watchlist")).find(function(currentMovie){
         return currentMovie.imdbID == imdbID;
     });
-    console.log(movie)
+    var watchedlistJSON = localStorage.getItem("watchedlist");
+    var watchedlist = JSON.parse(watchedlistJSON);
+    if(!watchedlist){
+        var watchedlist = []
+        };
+    watchedlist.push(movie);
+    watchedlistJSON = JSON.stringify(watchedlist);
+    localStorage.setItem("watchedlist", watchedlistJSON);
+    console.log(localStorage.getItem("watchedlist"));
+
+    var movieIndex = JSON.parse(localStorage.getItem("watchlist")).findIndex(function(currentMovie){
+        return currentMovie.imdbID == imdbID;
+    });
+    console.log(movieIndex)
     watchlist = JSON.parse(localStorage.getItem("watchlist"))
-    watchlist.splice(movie, 1);
+    watchlist.splice(movieIndex, 1);
     watchlistJSON = JSON.stringify(watchlist);
     localStorage.setItem("watchlist", watchlistJSON);
     document.getElementById("movies-container").innerHTML = renderMovies(JSON.parse(localStorage.getItem("watchlist")));
@@ -29,7 +43,25 @@ function renderMovies(movieArray) {
     return finalHTML;
 }
 
+function renderWatchedMovies(movieArray) {
+    var finalHTML = "";
+    var moviesHTML = movieArray.map(function(currentMovie){
+        var movieHTML = `<div class="card movie" id="movie-card" style="width: 18rem;">
+                    <img class="card-img-top" src="${currentMovie.Poster}" alt="movie poster">
+                    <div class="card-body">
+                      <h5 class="card-title">${currentMovie.Title}</h5>
+                      <p class="card-text">${currentMovie.Year}</p>
+                    </div>
+                  </div>`
+            return movieHTML
+    });
+
+    finalHTML += moviesHTML.join("");
+    return finalHTML;
+}
+
 document.addEventListener("DOMContentLoaded", function() {
     document.getElementById("movies-container").innerHTML = renderMovies(JSON.parse(localStorage.getItem("watchlist"))); 
+    document.getElementById("movies-watched").innerHTML = renderWatchedMovies(JSON.parse(localStorage.getItem("watchedlist")));
 });
 
